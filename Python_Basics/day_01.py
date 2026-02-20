8746c899a2e4b0567b5991112c549b5437cf6fff
Shivansh Garg <shivansh.g2005@gmail.com>
2026-02-20 18:17:40 +0530

    Renamed Day1.py to day_01.py

---
+++ Python_Basics/day_01.py
@@ -0,0 +1,14 @@
+#Day-1 of learning python for rest week-1
+print("Hello world") # Used Print() Function 
+
+# Input and Output in python 
+Name = input("Enter Your Name")
+print("Hello", Name,"Welcome To The Python World")
+
+s = "brad"
+print(s)
+
+s="shivansh Garg"
+age= 21
+city = "Ghaziabad"
+print(s,age,city)
