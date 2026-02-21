7deb60da4ee83aa60df2e55d304f89cf26aaa1e1
Shivansh Garg <shivansh.g2005@gmail.com>
2026-02-21 18:29:33 +0530

    Started Working on Day_04

---
+++ Python_Basics/day_04.py
@@ -0,0 +1,3 @@
+#Starting Out the Data Structures in python Depth - lists, sets, dictionary , tuple etc
+#Also knowing how access , insert , delete items in the following data structures
+#Also get to know operations to perform on data structures in python
