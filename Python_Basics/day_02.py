b5b4f8f946c91a009fcef40d7b78d20d1b373d23
Shivansh Garg <shivansh.g2005@gmail.com>
2026-02-21 17:34:42 +0530

    changes created

---
+++ Python_Basics/day_02.py
@@ -0,0 +1,52 @@
+# Variables Practice exercise
+# Clean Examples 
+name = "Shivansh"
+age = 21
+city = "ghaziabadh"
+
+print(name)
+print(age)
+print(city)
+
+# Multiple Variable 
+name = "sai"
+age = 21
+city = "noida"
+
+print("Name:" ,   name)
+print("Age:"  , age)
+print("City:" , city)
+
+# Variable Operations 
+a = 10 
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+
+
+#Exercises 
+my_name = "Shivansh Garg"
+my_college = "ganga college"
+my_age = 21
+
+print("Name : ",my_name," ","College : ",my_college)
+
+# Creating two numbers to perform variable operations 
+
+c = 100
+d = 50
+
+print(c +d)
+print(c-d)
+print(c*d)
+print(c/d)
+
+#Swapping two variables 
+e = 10
+f = 5
+a, b, c = a + 1, b + 2, c + 3
+print(a,b,c)
